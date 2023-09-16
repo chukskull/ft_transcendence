e98713016c99f40218eec027bf55eb918228c820
Saleh Nagat <snagat@e3r2p7.1337.ma>
2023-09-16 20:04:00 +0100

    uploading our fonts and setting some navbar layout

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Image from "next/image";
+
+// import CustomButton from "./CustomButton";
+
+const NavBar = () => (
+  <div className= 'flex items-center justify-between  padding-x h-full bg-red-500'>
+    <a href="#" className="Header-Logo">
+        <div>
+            <Image src="/assets/next.svg" alt="temp-logo"
+            width={100}
+            height={100}/>
+        </div>
+    </a>
+    <ul className="Navigation">
+      {
+          nav.map(({ name, href }) => (
+              <li key={name}>
+                  <a href={href}>
+                      <a>{name}</a>
+                  </a>
+              </li>
+          ))
+      }
+        
+    </ul>
+      <div>
+          am black
+      </div>
+     
+  </div>
+);
+const nav = [
+    {
+        name: "Home",
+        href: "/",
+    },
+    {
+        name: "About",
+        href: "/about",
+    },
+    {
+        name: "team",
+        href: "/team",
+    },
+];
+export default NavBar;
