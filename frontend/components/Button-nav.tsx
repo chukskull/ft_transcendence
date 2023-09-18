572927f6059ddce14bb602056cbe8f1e9c94aa95
Saleh Nagat <thuglifevideo7@gmail.com>
2023-09-18 20:22:07 +0100

    today task is done

--- frontend/components/Button-nav.tsx
+++ frontend/components/Button-nav.tsx
@@ -16,14 +16,14 @@ export const ButtonNav = () => {
       type="button"
       className={`flex items-center z-10 justify-between border-2 border-purple-600 
       rounded-2xl bg-none font-[ClashGrotesk-Medium] text-[15px]
-      py-[14px] px-[16px] text-sm font-medium text-gray-500 group
+      py-[14px] px-[16px] text-sm font-medium text-gray-500 transition group 
       ${isHovered ? "text-white" : ""}
       `}
       onMouseEnter={handleHover}
       onMouseLeave={handleHover}
     >
       Play Now
-      <span className={`ml-3 ${isHovered ? "group-hover:translate-x-1.5" : ""}`}>
+      <span className={`ml-3 ${isHovered ? " transition group-hover:translate-x-1.5" : ""}`}>
         <BiRightArrowAlt size={20}  style={{ color: "none" }} />
       </span>
     </button>
--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -6,16 +6,23 @@ export const Hero = () => {
     <>
       <div className="backrnd-img relative">
         <Image
-          width={1411}
-          height={1069}
-          className="z-[-10]"
+          width={1183.963}
+          height={796.884}
+          className="-z-[-10]"
           src={"/assets/frontshapes.png"}
           alt={"background shapes"}
         />
-        <div className="text-bc bg-none absolute top-[0%] xl:right-[52%] 2xl:right-[52%] my-[11.25rem] m-auto">
-          <h1 className="bg-none text-white my-2 w-[604px] font-[ClashGrotesk] text-8xl font-style-normal font-weight-600 p-[1.25rem]">
-            BEST ON DEMAND!
+        <div className=" flex flex-col gap-y-12 text-bc bg-none absolute top-[0%] xl:right-[50%] 2xl:right-[50%] my-[11.25rem]  p-[1.25rem]">
+          <h1 className="bg-none text-white my-2 w-[604px] font-[Josefin Sans] text-8xl font-style-normal font-medium">
+            BEST <br></br>ON DEMAND!
           </h1>
+          <p className="bg-none text-white w-[576px] font-[ClashGrotesk] text-lg font-medium">
+            {" "}
+            Step into the fast-paced and addictive world of ping pong Game! Get
+            ready to experience the thrill of virtual table tennis from the
+            comfort of your own device.
+          </p>
+          
         </div>
       </div>
     </>
