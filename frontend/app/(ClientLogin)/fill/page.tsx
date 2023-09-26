e9e488850cd17f47ab7d2d4fed6e06b5955a0a92
Saleh Nagat <thuglifevideo7@gmail.com>
2023-09-26 14:29:14 +0100

    Fill page clean && everything is fast

--- frontend/app/(ClientLogin)/fill/page.tsx
+++ frontend/app/(ClientLogin)/fill/page.tsx
@@ -4,53 +4,76 @@ import { Button, Input } from "@nextui-org/react";
 import React from "react";
 import "@/styles/globals.scss";
 
+
+const styles = {
+  label: "text-gray-400 dark:text-white/90 font-bold text-sm mb-2 pl-2",
+  input: [
+    "bg-transparent",
+    "text-white dark:text-white/90",
+    "placeholder:text-white dark:placeholder:text-white/60",
+    "placeholder-opacity-60",
+    "pl-2",
+    "font-bold",
+  ],
+  innerWrapper: "bg-transparent ",
+  inputWrapper: [
+    "w-80",
+    "h-[48px]",
+    "rounded-3xl",
+    "border-0",
+    "shadow-xl",
+    "bg-transparent",
+    "hover-within:text-black/80",
+    "backdrop-blur-xl",
+    "backdrop-saturate-200",
+    "!cursor-text",
+  ],
+};
+
+
 export default function Fill() {
-  const variants = ["flat"];
   return (
-    // <NextUIProvider>
     <>
       <div className="h-screen w-screen flex items-center justify-center">
-        <div className="rectangle flex flex-col items-center justify-center gap-14">
+        <div className="rectangle flex  items-center justify-center">
+          <div className="w-80 flex flex-col items-center justify-center gap-12">
+
           <Input
+          type="name"
+          label="Name"
             isClearable
-            radius="lg"
+            variant="bordered"
             classNames={{
-              label: "text-black/50 dark:text-white/90",
-              input: [
-                "bg-none",
-                "text-black/90 dark:text-white/90",
-                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-              ],
-              innerWrapper: "bg-none",
-              inputWrapper: [
-                "shadow-xl",
-                "bg-default-200/50",
-                "dark:bg-default/60",
-                "backdrop-blur-xl",
-                "backdrop-saturate-200",
-                "hover:bg-default-200/70",
-                "dark:hover:bg-none",
-                "group-data-[focused=true]:bg-none",
-                "dark:group-data-[focused=true]:bg-none",
-                "!cursor-text",
-              ],
+             ...styles,
             }}
           />
-        </div>
-        {/* <Input
-          color="primary"
-          className="w-3/4"
-          type="email"
-          variant="bordered"
-          label="Nick Name"
-        />
-        <Input
-          color="primary"
-          className="w-3/4"
-          type="email"
-          variant="bordered"
+          <Input
+          type="lastname"
           label="Last Name"
-        /> */}
+            isClearable
+            variant="bordered"
+            classNames={{
+             ...styles,
+            }}
+          />
+          <Input
+          type="nickname"
+          label="Nickname"
+            isClearable
+            variant="bordered"
+            classNames={{
+             ...styles,
+            }}
+          />
+           <Button
+            
+            className="mt-2 w-[140px] h-[40px] gradient-button 
+           text-white shadow-lg rounded-3xl"
+          >
+             Play
+          </Button>
+          </div>
+        </div>
       </div>
     </>
   );
