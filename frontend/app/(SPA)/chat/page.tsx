bcef48f83a725956a9a74c0e851687802decd8fd
mountassir-007 <ahmedelmountassir10@gmail.com>
2023-10-09 05:20:57 +0100

    addchannel and contact friend done

--- frontend/app/(SPA)/chat/page.tsx
+++ frontend/app/(SPA)/chat/page.tsx
@@ -1,68 +1,11 @@
 "use client";
 import style from "@/styles/SPA/chat/chat.module.scss";
-import React from "react";
 import "@/styles/globals.scss";
-import { FiLogOut } from "react-icons/fi";
 import { LiaTelegramPlane } from "react-icons/lia";
-import { useState } from "react";
 import ChannelsSection from "@/components/SPA/chat/ChannelsSection";
 import ChatHeader from "@/components/SPA/chat/ChatHeader";
-import DMbox from "@/components/SPA/chat/DMbox";
 import MsgsList from "@/components/SPA/chat/MessagesList";
-
-
-
-const dmList = [
-  {
-    name: "John Doe",
-    online: true,
-    lastMsg: "Hey, how are you?",
-    lastMsgTime: "02:00 PM",
-    avatar: "/assets/components/Profile.svg",
-  },
-  {
-    name: "John Doe",
-    online: false,
-    lastMsg: "Hey, how are you?",
-    lastMsgTime: "11:00 AM",
-    avatar: "/assets/components/Profile.svg",
-  },
-  {
-    name: "John Doe",
-    online: true,
-    lastMsg: "Hey, how are you?",
-    lastMsgTime: "12:00 PM",
-    avatar: "/assets/components/Profile.svg",
-  },
-  {
-    name: "John Doe",
-    online: false,
-    lastMsg: "Hey, how are you?",
-    lastMsgTime: "11:00 AM",
-    avatar: "/assets/components/Profile.svg",
-  },
-  {
-    name: "John Doe",
-    online: true,
-    lastMsg: "Hey, how are you?",
-    lastMsgTime: "12:00 PM",
-    avatar: "/assets/components/Profile.svg",
-  },
-  {
-    name: "John Doe",
-    online: false,
-    lastMsg: "Hey, how are you?",
-    lastMsgTime: "11:00 AM",
-    avatar: "/assets/components/Profile.svg",
-  },
-  {
-    name: "John Doe",
-    online: true,
-    lastMsg: "Hey, how are you?",
-    lastMsgTime: "12:00 PM",
-    avatar: "/assets/components/Profile.svg",
-  },
-];
+import DmSection from "@/components/SPA/chat/DMSection";
 
 const msgsdb = [
   {
@@ -102,24 +45,6 @@ const msgsdb = [
     avatar: "/assets/components/Profile.svg",
   },
 ];
-
-
-
-const DmSection = () => {
-  return (
-    <div className={style["direct-msgs"]}>
-      <div className={style["section-header"]}>
-        <h2>DIRECT MESSAGES</h2>
-        <button className={style["add-btn"]}>+</button>
-      </div>
-      <div className={style["dm-list"]}>
-        {dmList.map((dm) => (
-          <DMbox dm={dm} key={dm.name} />
-        ))}
-      </div>
-    </div>
-  );
-};
 
 export default function Chat() {
   return (
