23e2f05a69c01fa628b96eb1f0af386ff840c52b
me2 <me>
2023-10-20 13:37:52 +0100

    pushing backend into dev

---
+++ backend/src/chat/message/message.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty } from "class-validator";
+
+export class messageDto {
+  msgId: number;
+
+  @IsNotEmpty()
+  channelId: number;
+
+  @IsNotEmpty()
+  senderId: number;
+
+  @IsNotEmpty()
+  content: string;
+
+  @IsNotEmpty()
+  timestamp: string;
+}
