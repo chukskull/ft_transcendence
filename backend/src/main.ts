7ca2a87f4e03af4c5ac5993a47229bf34434d44a
me2 <me>
2023-11-07 03:55:24 +0100

    removed WebSockets in order to migrate to sockets.io

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
-import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
   app.use(cookieParser());
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(new ValidationPipe());
