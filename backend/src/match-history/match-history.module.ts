5868ea077c17026f45b0fca9cf44fc361beefd93
Achraf Maghous <acmaghou@e2r8p7.1337.ma>
2023-11-18 18:04:53 +0100

    trying to fix database connectivity

--- backend/src/match-history/match-history.module.ts
+++ backend/src/match-history/match-history.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MatchHistory } from './entities/match-history.entity';
 import { MatchHistoryService } from './match-history.service';
 import { MatchHistoryController } from './match-history.controller';
+import { User } from 'src/user/user.entity';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([MatchHistory, User])],
+	providers: [MatchHistoryService],
 	controllers: [MatchHistoryController],
-	providers: [MatchHistoryService], // Added a closing parenthesis here
 })
 
 export class MatchHistoryModule {}
