eaa105a4eaae90195cfbb74cff33f0ae408189ae
mountassir-007 <ahmedelmountassir10@gmail.com>
2023-11-18 18:45:59 +0100

    everything is sync

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
-// import { AuthService } from 'src/user/auth/auth.service';
 import { UserService } from './user.service';
 import { JwtModule } from '@nestjs/jwt';
-// import { jwtConstants } from './auth/models/constants';
 import { Channel } from '../channel/channel.entity';
 import { Conversation } from '../conversations/conversation.entity';
 import { Achievement } from '../achievement/achievement.entity';
-import { AuthModule } from '../auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
 import { FortyTwoStrategy } from 'src/auth/auth.strategy';
 import { PassportModule } from '@nestjs/passport';
@@ -20,7 +17,7 @@ import { PassportModule } from '@nestjs/passport';
     JwtModule.register({
       secret: 'f439843--213+@y4t34u',
       signOptions: { expiresIn: '30d' },
-    })
+    }),
   ],
   controllers: [UserController],
   providers: [UserService, AuthService, FortyTwoStrategy],
