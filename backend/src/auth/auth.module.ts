6f65fbac2459d7d1e87493e338247829ca3790d0
mountassir-007 <ahmedelmountassir10@gmail.com>
2023-11-20 21:51:00 +0100

    notif fixes

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -14,6 +14,7 @@ import { PassportModule } from '@nestjs/passport';
     JwtModule.register({
       secret: 'f439843--213+@y4t34u',
       signOptions: { expiresIn: '30d' },
+      
     }),
   ],
   providers: [AuthService, FortyTwoStrategy],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import * as passport from 'passport';
+import { serialize } from 'v8';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(passport.initialize());
-  // app.use(passport.session());
+  app.use(passport.session());
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
