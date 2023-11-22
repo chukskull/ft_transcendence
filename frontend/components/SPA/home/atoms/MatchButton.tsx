49f2427cb661584142b9d7a125dd2d3586135f05
Achraf Maghous <acmaghou@e2r8p2.1337.ma>
2023-11-22 10:39:38 +0100

    checking sockets

--- frontend/components/SPA/home/atoms/MatchButton.tsx
+++ frontend/components/SPA/home/atoms/MatchButton.tsx
@@ -1,13 +1,21 @@
 import { Button } from "@nextui-org/react";
 import React from "react";
+import io from "socket.io-client";
 
 export const MatchButton = () => {
+  const socket = io();
+
+  const handleJoinQueue = () => {
+    socket.emit("joinQueue");
+  };
+
   return (
     <div>
       <Button
         className="bg-live text-white font-semibold text-base max-w-[239px] transition duration-500 ease-in-out hover:scale-105 hover:bg-opacity-80"
         data-hover
         data-focus
+        onClick={handleJoinQueue}
       >
         JOIN MATCHMAKING
       </Button>
