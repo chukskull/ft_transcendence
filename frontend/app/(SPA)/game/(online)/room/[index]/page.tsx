1c26330ffd3e0320130666da7ca265a09625fa5c
chuskull <thuglifevideo7@gmail.com>
2023-11-25 22:49:19 +0100

    creating route for game

---
+++ frontend/app/(SPA)/game/(online)/room/[index]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import OnlineGame from "@/components/SPA/game/OnlineGame"
+
+export const GameOnline = () => {
+  //get the map idk
+  //get index idk
+  return (
+    <OnlineGame/>;
+  )
+}
