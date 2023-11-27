de6102841271c5b83267d384b6b73ef4acf7191c
Achraf Maghous <acmaghou@e1r4p9.1337.ma>
2023-11-27 18:49:08 +0100

    back from oldcommit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["i.imgur.com", "utfs.io"],
-  },
+	images: {
+		domains: ["i.imgur.com", "utfs.io"],
+	},
 
 };
-
 module.exports = nextConfig;
