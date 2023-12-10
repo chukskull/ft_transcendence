d11c4546c2a943682a1fd3bb2a8ff10bca2310b2
Buff Achraf <buffAchraf>
2023-12-10 14:15:30 +0100

    fixed math game

--- frontend/components/SPA/game/TheGame.tsx
+++ frontend/components/SPA/game/TheGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, use } from "react";
 import style from "@/styles/SPA/game/game.module.scss";
 import io from "socket.io-client";
 
@@ -6,6 +6,11 @@ type Score = {
   player: number;
   ai: number;
 };
+
+const DIST_WALL_TO_PADDLE = 20;
+const PADDLE_HEIGHT = 110;
+const PADDLE_WIDTH = 13;
+const BALL_RADIUS = 16;
 
 const useKeyHandler = () => {
   const [keys, setKeys] = useState<Record<string, boolean>>({});
@@ -35,7 +40,6 @@ export default function TheGame({ map }: { map: string }) {
   const [gameStarted, setGameStarted] = useState(false);
   const canvasWidth: number = 860;
   const canvasHeight: number = 500;
-  const ballDiameter: number = 16;
   const [score, setScore] = useState<Score>({ player: 0, ai: 0 });
   const [ball, setBall] = useState({
     x: 417,
@@ -55,36 +59,32 @@ export default function TheGame({ map }: { map: string }) {
     }
 
     const gameLoop = () => {
-      // Update paddle position
-      if (keys["ArrowUp"] && playerPaddleY > 0) {
-        setPlayerPaddleY(playerPaddleY - 5);
-      }
-      if (keys["ArrowDown"] && playerPaddleY + 110 < canvasHeight) {
-        setPlayerPaddleY(playerPaddleY + 5);
-      }
+      
 
-      // Ball movement
-      setBall((prevBall) => ({
-        ...prevBall,
-        x: prevBall.x + prevBall.speedX,
-        y: prevBall.y + prevBall.speedY,
-      }));
+      // Check for ball going out of left/right walls
+      const hitRightEdge = ball.x > canvasWidth - PADDLE_WIDTH;
+      const hitLeftEdge = ball.x <= 5;
 
-      // Check for ball going out of horizontal walls
-      if (ball.x + ball.speedX > canvasWidth || ball.x + ball.speedX < 15) {
-        if (ball.x + ball.speedX > canvasWidth) {
-          setScore((prev) => ({ ...prev, player: prev.player + 1 }));
-        } else if (ball.x + ball.speedX < 15) {
-          setScore((prev) => ({ ...prev, ai: prev.ai + 1 }));
-        }
+      if (hitRightEdge || hitLeftEdge) {
+        setScore((prev) => ({
+          ...prev,
+          player: hitRightEdge ? prev.player + 1 : prev.player,
+          ai: hitLeftEdge ? prev.ai + 1 : prev.ai
+        }));
+        console.log("=> 3");
+
         setBall({
           x: 417,
           y: 240,
-          speedX: 2,
-          speedY: 2,
+          speedX: 3,
+          speedY: 3,
         });
+        console.log("=> 4");
         return;
       }
+
+      // return;
+      // }
 
       // Ball collisions with top and bottom walls
       if (
@@ -92,34 +92,80 @@ export default function TheGame({ map }: { map: string }) {
         ball.y + ball.speedY < -3
       ) {
         setBall((prevBall) => ({ ...prevBall, speedY: -prevBall.speedY }));
+        // console.log("=> 5");
       }
 
-      // Ball collisions with paddles
-      if (
-        ball.x + ball.speedX > canvasWidth - 53 ||
-        (ball.x + ball.speedX < 25 &&
-          ball.y >= playerPaddleY &&
-          ball.y <= playerPaddleY + 110)
-      ) {
+      // Ball collisions with paddle
+      const hitRightPaddle = ball.x >= canvasWidth - (PADDLE_WIDTH + DIST_WALL_TO_PADDLE + BALL_RADIUS);
+      const hitLeftPaddle = ball.x <= DIST_WALL_TO_PADDLE && ball.y >= playerPaddleY && ball.y <= playerPaddleY + PADDLE_HEIGHT;
+      if (hitLeftPaddle || hitRightPaddle) {
         // Increase ball speed on paddle collision
-        const increasedSpeedX = -ball.speedX * 1.05; // Increase speed by a factor (e.g., 1.2)
-        setBall((prevBall) => ({ ...prevBall, speedX: increasedSpeedX }));
+        console.log(`{hitLeftPaddle: ${hitLeftPaddle}, hitRightPaddle: ${hitRightPaddle}}, {ball.speedX: ${ball.speedX}}`)
+        if ((hitLeftPaddle && ball.speedX < 0) || (hitRightPaddle && ball.speedX >= 0)) {
+          console.log(` changed ===> {hitLeftPaddle: ${hitLeftPaddle}, hitRightPaddle: ${hitRightPaddle}}, {ball.speedX: ${ball.speedX}}`)
+          if (ball.speedX * ball.speedX < 81) {
+            const increasedSpeedX = -ball.speedX * 1.2; // Increase speed by a factor (e.g., 1.05)
+            setBall((prevBall) => ({ ...prevBall, speedX: increasedSpeedX }));
+            console.log("=> 6");
+          } else {
+            setBall((prevBall) => ({ ...prevBall, speedX: -ball.speedX }));
+            console.log("=> 7");
+          }
+        }
       }
 
-      // Update AI paddle position based on ball's y-coordinate
-      if (EnemyPaddleY + 40 < ball.y && EnemyPaddleY + 110 < canvasHeight) {
-        setEnemyPaddleY(EnemyPaddleY + 2);
-      } else if (EnemyPaddleY + 40 > ball.y && EnemyPaddleY > 5) {
-        setEnemyPaddleY(EnemyPaddleY - 2);
-      }
+      /**
+       * 
+       * Looks like the reason the ball is bouncing before hitting the paddle  is because of setting the ball x position to a big value or small value using speedX
+       * hitLeftPadlle can be triggered multiple times ater the first hit because the condition is true for multiple frames
+       * thus we should check first if the ball has already hit (by checking the speedX sign)
+       */
+      // Ball movement
+      setBall((prevBall) => ({
+        ...prevBall,
+        x: prevBall.x + (hitLeftPaddle || hitRightPaddle ? (hitLeftPaddle ? 0.3 : -0.3) * BALL_RADIUS : prevBall.speedX),
+        y: prevBall.y + prevBall.speedY,
+      }));
+      // ball.speedX * ball.speedX >= 55 && console.log(`=> 8 {ball.x: ${ball.x} hitLeftPaddle: ${hitLeftPaddle}, hitRightPaddle: ${hitRightPaddle}}`);
     };
 
-    const gameInterval = setInterval(gameLoop, 10);
+    const gameInterval = setInterval(gameLoop, 17 ); // Prblem appears only when the debugger is of (not sure why) => the higher less the problem
+    /**
+     * I guess the problem is setting state is async and sometimes it skips the state where it hit the paddle
+     */
 
     return () => {
       clearInterval(gameInterval);
     };
-  }, [ball, playerPaddleY, EnemyPaddleY, keys, gameStarted]);
+  }, [ball.x, ball.y, gameStarted]);
+
+  useEffect(() => {
+    // handle ai paddle
+    if (!gameStarted) {
+      return;
+    }
+
+    // Update AI paddle position based on ball's y-coordinate
+    if (EnemyPaddleY + 40 < ball.y && EnemyPaddleY + PADDLE_HEIGHT < canvasHeight) {
+      setEnemyPaddleY(EnemyPaddleY + 4);
+    } else if (EnemyPaddleY + 40 > ball.y && EnemyPaddleY > 5) {
+      setEnemyPaddleY(EnemyPaddleY - 4);
+    }
+
+  }, [gameStarted, ball.y, EnemyPaddleY]);
+
+  useEffect(() => {
+    if (!gameStarted) {
+      return;
+    }
+
+    // handle player paddle
+    if (keys["ArrowDown"] && playerPaddleY + PADDLE_HEIGHT < canvasHeight) {
+      setPlayerPaddleY(playerPaddleY + 12);
+    } else if (keys["ArrowUp"] && playerPaddleY > 5) {
+      setPlayerPaddleY(playerPaddleY - 12);
+    }
+  }, [gameStarted, keys]);
 
   const handleStartGame = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key === " ") {
