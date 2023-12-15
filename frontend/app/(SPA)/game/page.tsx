f307b951ddf02fbd9d7eea8907a521d404d90e77
achraf <achraf>
2023-12-15 17:11:56 +0100

    fixing winner error

--- frontend/app/(SPA)/game/page.tsx
+++ frontend/app/(SPA)/game/page.tsx
@@ -35,6 +35,7 @@ const Game: React.FC = () => {
   }
   socket?.on("changeState", (data: any) => {
     if (data.status == "inQueue") {
+      console.log("in queue", socket.id);
       setJoinedQueue(true);
     } else if (data.status == "failed") {
       // popus that user is already in queue in another window
